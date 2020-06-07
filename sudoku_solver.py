12540a900d5edfda5e7e7680fbb09b809df42735
haliechm <haliechm@live.unc.edu>
2020-06-06 22:58:33 -0400

    Added ability to grab text values

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -6,7 +6,7 @@ class Solver(wx.Frame):
         # frame = wx.Frame(None, -1,)
         # frame.SetMinSize(wx.Size(750, 750))
         # frame.SetMaxSize(wx.Size(750, 750))
-
+        self.my_controls = []
         self.Bind(wx.EVT_PAINT, self.OnPaint)
         # self.OnPaint()
         self.InitUI()
@@ -14,7 +14,40 @@ class Solver(wx.Frame):
         self.Show()
 
     def onClick(self, event):
-        print("here")
+
+
+        print(self.my_controls[0].GetValue())
+
+        row1 = []
+        row2 = []
+        row3 = []
+        row4 = []
+        row5 = []
+        row6 = []
+        row7 = []
+        row8 = []
+        row9 = []
+        for i in range(81):
+            if i <= 8:
+                row1.append(self.my_controls[i].GetValue())
+            elif i <= 17:
+                row2.append(self.my_controls[i].GetValue())
+            elif i <= 26:
+                row3.append(self.my_controls[i].GetValue())
+            elif  i<= 35:
+                row4.append(self.my_controls[i].GetValue())
+            elif i <= 44:
+                row5.append(self.my_controls[i].GetValue())
+            elif i <= 53:
+                row6.append(self.my_controls[i].GetValue())
+            elif i <= 61:
+                row7.append(self.my_controls[i].GetValue())
+            elif i <= 70:
+                row8.append(self.my_controls[i].GetValue())
+            else:
+                row9.append(self.my_controls[i].GetValue())
+        #solve puzzle here
+        #then reveal in red the answers
 
     def InitUI(self):
         # menubar = wx.MenuBar()
@@ -35,9 +68,12 @@ class Solver(wx.Frame):
         # dc.DrawLine(10, 10, 50, 50)
         self.SetFont(wx.Font(28, wx.SWISS, wx.NORMAL, wx.NORMAL, False,'MS Shell Dlg 2'))
         for i in range(81):
-            #gs.Add(wx.ComboBox(self, 15, "X", choices=["X", "1", "2", "3", "4", "5", "6", "7", "8", "9"], style = wx.CB_READONLY))
+            #gs.Add(wx.ComboBox(self, 15, "X", choices=["X", "1", "2", "3", "4", "5", "6", "7", "8", "9"], style = wx.CB_READONLY)
+            text_control = wx.TextCtrl(self, id==1, value="", size=(88,88), style = wx.TE_CENTRE)
 
-            gs.Add(wx.TextCtrl(self, id==1, value="", size=(88,88), style = wx.TE_CENTRE))
+            self.my_controls.append(text_control)
+            #gs.Add(wx.TextCtrl(self, id==1, value="", size=(88,88), style = wx.TE_CENTRE))
+            gs.Add(text_control)
 
         self.SetFont(wx.Font(12, wx.SWISS, wx.NORMAL, wx.NORMAL, False,'MS Shell Dlg 2'))
         butt = wx.Button(self, wx.ID_ANY, "Click to Solve Puzzle", (320, 730))
@@ -82,7 +118,7 @@ class Solver(wx.Frame):
 
             dc.DrawLine(0, y, 792, y)
             y += 88
-            j += 1
+
 
 
         # dc.DrawLine(0, 0, 50, 50)
